2bbdc301cffd5bb1bfbb612a225ff58ae5bb1272
Ebenezer Okello <okelloebenezer403@gmail.com>
2023-07-23 17:50:23 +0300

    hhh

--- models/order.js
+++ models/order.js
@@ -15,6 +15,6 @@ const orderSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-const Order = mongoose.model("Order", orderSchema);
+const Order = mongoose.model("Orders", orderSchema);
 
 exports.Order = Order;
--- routes/orders.js
+++ routes/orders.js
@@ -1,4 +1,4 @@
-const { Order } = require("../models/order")
+const { Order } = require("../models/Order")
 const { auth1, isUser, isAdmin } = require("../middleware/auth");
 const moment = require('moment')
 
