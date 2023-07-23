b9bbe3f31514c94b4a373676e37d426613c1da92
Ebenezer Okello <okelloebenezer403@gmail.com>
2023-07-23 17:41:38 +0300

    ggg

--- routes/orders.js
+++ routes/orders.js
@@ -1,4 +1,4 @@
-const { Order } = require("../models/Order");
+const { Order } = require("../models/order")
 const { auth1, isUser, isAdmin } = require("../middleware/auth");
 const moment = require('moment')
 
